fd5138d70aab48aa52cd977cc3d98680c54f7c48
MariOdy <75803429+MariOdy@users.noreply.github.com>
2023-05-18 18:48:26 +0300

    Add Hero component

---
+++ components/Hero/Hero.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Image, { StaticImageData } from "next/image";
+import HeroHome from "@/public/hero-home.png";
+
+import styles from "./styles.module.scss";
+import { fraunces } from "@/app/layout";
+
+const Hero: React.FC = () => {
+  return (
+    <div className={styles.hero_wrapper}>
+      <Image src={HeroHome} alt="Coffee with kettle on table" fill priority />
+      <div>
+        <h2 className={fraunces.className}>Great coffee made simple.</h2>
+        <p>
+          Start your mornings with the world&apos;s best coffees. Try our
+          expertly curated artisan coffees from our best roasters delivered
+          directly to your door, at your schedule.
+        </p>
+        <button>create your plan</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
